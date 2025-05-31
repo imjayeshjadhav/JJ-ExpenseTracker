d848f1de2dd4571f8f45d09db17d951bca3af520
imjayeshjadhav <14jayeshjadhav@gmail.com>
2025-05-31 14:00:49 +0530

    Sql database initialization using Neon

---
+++ backend/config/db.js
@@ -0,0 +1,6 @@
+import {neon} from "@neondatabase/serverless"
+import "dotenv/config"
+
+// create a sql connection
+export const sql = neon(process.env.DATABASE_URI)
+
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,33 @@
 import express from "express"
 import dotenv from "dotenv"
+import { sql } from "./config/db.js"
+
 const app= express()
 
 dotenv.config()
 
 const PORT = process.env.PORT 
 
-app.listen(PORT, ()=>{
+async function initDB(){
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL (10,2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+
+        console.log("Database initialised successfully")
+    } catch (error) {
+        console.log("Error initializing DB", error)
+        process.exit(1)
+    }
+}
+
+initDB().then(()=>{
+    app.listen(PORT, ()=>{
     console.log(`Server is running on PORT ${PORT}`)
+    })
 })
